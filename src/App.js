e64c7376ea15c200deeda66c7fcaaec94467700c
dewdot <ravi.9793@gmail.com>
2021-10-17 11:44:27 +0530

    removed demo link

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <>
       <div className="container">
         <Tab />
-        <div className="footer-links"><a href="https://github.com/dewdot/react-tab">GitHub Project</a> | <a href="#">Live Demo</a></div>
+        <div className="footer-links"><a href="https://github.com/dewdot/react-tab">GitHub Project</a> | <a href="http://localhost:3000/">Live Demo</a></div>
       </div>
     </>
 
